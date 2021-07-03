7161f6135a9490b0a282c37346a6d38c24ebe1e2
ricardo-dota <58540274+ricardo-dota@users.noreply.github.com>
2021-07-03 17:00:45 -0400

    sistema de login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .forms import RegistrationForm
+from .models import Account
+from django.http import HttpResponse
+from django.contrib import messages , auth
 # Create your views here.
+
+def register(request):
+    #comprobamos si se envio algo
+    if request.method == 'POST':
+        form  =  RegistrationForm(request.POST)
+        if form.is_valid():
+            first_name      = form.cleaned_data['first_name']
+            last_name       = form.cleaned_data['last_name'] 
+            email           = form.cleaned_data['email']
+            phone_number    = form.cleaned_data['phone_number'] 
+            password        = form.cleaned_data['password']
+            username        = email.split("@")[0]
+            user = Account.objects.create_user( first_name = first_name, last_name= last_name , email =  email  , username = username,password = password)
+            user.phone_number = phone_number
+            user.save()
+            messages.success(request, 'Registro exitoso')
+            return redirect('registers')
+        else:
+            messages.error(request, 'Tu Formulario contiene Errores')
+            #exit()
+    else:
+        form = RegistrationForm()
+    context = {
+        'form' : form
+    }
+    return render(request,'accounts/register.html',context)
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate( email = email , password = password )
+        if user is not None:
+            auth.login(request, user)
+            messages.success( request , 'Estas logeado')
+            return redirect('home')
+        else:
+            messages.error( request , 'Credenciales invalidas')
+            return redirect('login')
+    return render(request,'accounts/login.html')
+
+def logout(request):
+    return
